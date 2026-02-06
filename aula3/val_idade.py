95cdbb78d25f22fe12f626365cd6d16fe5faf9ce
danielmacielbenicio <danielmacielbenicio@gmail.com>
2026-02-05 21:46:33 -0300

    fix: fixed bugs in aula3

--- aula3/val_idade.py
+++ aula3/val_idade.py
@@ -1,14 +1,15 @@
-# estou confuso nesta lição. Irei analisar mais os booleans
+idade = (input("Digite a sua idade: "))
+tem_carteira = (input("Você tem carteira?"))
+doc = bool
 
-idade = int(input("Digite a sua idade: "))
-tem_carteira = bool
+if tem_carteira == "s" or tem_carteira == "S":
+    doc = True
+elif tem_carteira == "n" or tem_carteira == "N":
+    doc = False
+else:
+    print("ERRO: Operação Inválida")
 
-# AND: ambas as condições precisam ser verdadeiras
-print(idade >= 18 and tem_carteira)  # True
-
-# OR: uma das condições precisa ser verdadeira
-print(idade >= 18 or tem_carteira)   # True
-
-# NOT: inverte o valor
-print(not tem_carteira)              # False
-
+if doc and idade >= 18:
+    print("Acesso permitido!")
+else:
+    print("Acesso negado!")
---
+++ aula3/val_senha.py
@@ -0,0 +1,8 @@
+senha = (input("Digite o PIN: "))
+chave_acesso = ("0192837465")
+
+if senha == chave_acesso:
+    print("Acesso permitido!")
+else:
+    print("Acesso negado!")
+
